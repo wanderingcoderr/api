ee72629ffb44248b4ab74384d49a432a08d4934d
Junaid <junaidvayoli@gmail.com>
2020-04-04 02:40:22 +0900

    bug fix

--- sheet-to-json_generic.js
+++ sheet-to-json_generic.js
@@ -9,7 +9,8 @@ const c = require("./lib/constants");
   await task({ sheet: c.SHEET, tabs:{travel_history: c.SHEET_TRAVEL_HISTORY}, file: c.FILE_TRAVEL_HISTORY});
   await task({ sheet: c.SHEET, tabs:{factoids: c.SHEET_NAME_FACTOIDS, faq: c.SHEET_FAQ}, file: c.FILE_WEBSITE_DATA});
   await task({ sheet: c.SHEET, tabs:{faq: c.SHEET_FAQ}, file: c.FILE_FAQ}); // This will be removed in the future. use website_data json for future
-  await task({ sheet: c.SHEET, tabs:{states_tested_data: c.SHEET_StateWise_Tested_Numbers_Data}, file: c.FILE_DATE_WISE_DELTA});
+  await task({ sheet: c.SHEET, tabs:{states_tested_data: c.SHEET_StateWise_Tested_Numbers_Data}, file: c.FILE_STATEWISE_TESTED_DATA});
+  await task({ sheet: c.SHEET, tabs:{states_daily: c.SHEET_DATE_WISE_DELTA}, file: c.FILE_DATE_WISE_DELTA});
   console.log("Created Json File With Updated Contents");
 }) ();
 
